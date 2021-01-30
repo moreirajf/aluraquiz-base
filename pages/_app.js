697821ef9558135ff89901f71391b8cbe29e949d
moreirajf <jfranciscomoreira@gmail.com>
2021-01-30 20:38:49 -0300

    aulas 2 e 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
+
 const GlobalStyle = createGlobalStyle`
   *{
     box-sizing: border-box;
@@ -11,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
     flex-direction: column;
     font-family: 'Lato', sans-serif;
 
-    color: ${({theme})=> theme.colors.contrastText};
+    color: ${({ theme }) => theme.colors.contrastText};
   }
 
   html, body {
@@ -22,18 +27,22 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      
+
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-          <Component {...pageProps} />
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import styled from 'styled-components'
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widgets';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-Image: url(${db.bg});
@@ -23,19 +33,42 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
   return (
-    <QuizBackground backgroundImage = {db.bg}>
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>CSS Is Awesome</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Loren ypssdsdasdfsfs...</p>
-          </Widget.Content>  
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('submissao por react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}  
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
         </Widget>
 
         <Widget>
@@ -47,10 +80,9 @@ export default function Home() {
           </Widget.Content>
         </Widget>
         <Footer />
-        <GitHubCorner projectUrl="https://github.com/moreirajf"/>
+        <GitHubCorner projectUrl="https://github.com/moreirajf" />
       </QuizContainer>
     </QuizBackground>
 
   );
-
 }
